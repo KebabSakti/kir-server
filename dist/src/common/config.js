513b1c222efb4e36cdc5e4cdbd0024cfcd4d55f1
Julian Aryo <julian.aryo1989@gmail.com>
2024-08-28 10:44:54 +0800

    ADD KNEX ADAPTER

--- dist/src/common/config.js
+++ dist/src/common/config.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.staticDir = exports.pub = exports.server = void 0;
-exports.server = "https://25d1-36-82-197-168.ngrok-free.app"; //"http://192.168.10.32:3000";
-exports.pub = "https://25d1-36-82-197-168.ngrok-free.app"; //"http://192.168.10.32:3000";
+exports.server = "https://server.ujiberkala-dstj-dishub.my.id";
+exports.pub = "https://ujiberkala-dstj-dishub.my.id";
 exports.staticDir = "src/asset/upload";
--- src/common/config.ts
+++ src/common/config.ts
@@ -1,3 +1,3 @@
-export const server = "http://192.168.10.32:3000"; //"https://25d1-36-82-197-168.ngrok-free.app"; //"http://192.168.10.32:3000";
-export const pub = "http://192.168.10.32:3000"; //"https://25d1-36-82-197-168.ngrok-free.app"; //"http://192.168.10.32:3000";
+export const server = "https://server.ujiberkala-dstj-dishub.my.id";
+export const pub = "https://ujiberkala-dstj-dishub.my.id";
 export const staticDir = "src/asset/upload";
